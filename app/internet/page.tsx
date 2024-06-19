6122934a5c512b0a6cb53f2319db87679cad486b
aiacuone <aiacuone@gmail.com>
2024-06-19 16:42:52 +0100

    stop tracking .env remove console log

--- app/internet/page.tsx
+++ app/internet/page.tsx
@@ -4,11 +4,6 @@ import { CustomTabs } from '@/components/ui/CustomTabs'
 import { internetTabs } from './data'
 
 const InternetPage = () => {
-  console.log({
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  })
   return <CustomTabs tabs={internetTabs} />
 }
 
