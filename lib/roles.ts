f80a7e691f1522053e4ab86e49f1a44a6b3712d9
aiacuone <aiacuone@gmail.com>
2024-06-22 14:05:26 +0100

    add as a tenant

--- lib/roles.ts
+++ lib/roles.ts
@@ -2,7 +2,7 @@ import { Role } from './types/user'
 
 export const roles: Record<Role, string[]> = {
   [Role.admin]: ['aiacuone@gmail.com'],
-  [Role.tenant]: [],
+  [Role.tenant]: ['dahya.d@gmail.com'],
   [Role.agent]: [],
 }
 
