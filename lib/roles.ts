6c3110fcd590eeecfec7005f51c9b905d59e4e0d
aiacuone <aiacuone@gmail.com>
2024-06-22 15:54:43 +0100

    added Marina as agent, changed Dipi to admin

--- lib/roles.ts
+++ lib/roles.ts
@@ -1,9 +1,9 @@
 import { Role } from './types/user'
 
 export const roles: Record<Role, string[]> = {
-  [Role.admin]: ['aiacuone@gmail.com'],
-  [Role.tenant]: ['dahya.d@gmail.com'],
-  [Role.agent]: [],
+  [Role.admin]: ['aiacuone@gmail.com', 'dahya.d@gmail.com'],
+  [Role.tenant]: [],
+  [Role.agent]: ['marina.carter@wardsofkent.co.uk'],
 }
 
 // 'dahya.d@gmail.com'
